a408188a4b8ef46d3b6a4cc605e8dd8a51b5e1d2
Vinicius <viniholtman28@gmail.com>
2022-11-27 17:29:09 -0300

    Created test to create a new statement

---
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -0,0 +1,42 @@
+import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "../../../users/useCases/createUser/CreateUserUseCase";
+import { ICreateUserDTO } from "../../../users/useCases/createUser/ICreateUserDTO";
+import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
+import { CreateStatementError } from "./CreateStatementError";
+import { CreateStatementUseCase } from "./CreateStatementUseCase";
+
+let createUserUseCase: CreateUserUseCase;
+let createStatementUseCase: CreateStatementUseCase;
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let inMemoryStatementsRepository: InMemoryStatementsRepository;
+
+describe("Create Statement", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    inMemoryStatementsRepository = new InMemoryStatementsRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+    createStatementUseCase = new CreateStatementUseCase(
+      inMemoryUsersRepository,
+      inMemoryStatementsRepository
+    );
+  });
+
+  it("should be able to create a new statement", async () => {
+    const user: ICreateUserDTO = {
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+      password: "12345",
+    };
+
+    const { id } = await createUserUseCase.execute(user);
+
+    const statement = await createStatementUseCase.execute({
+      user_id: id as string,
+      type: "deposit" as any,
+      amount: 1000,
+      description: "test description",
+    });
+
+    expect(statement).toHaveProperty("id");
+  });
+});
